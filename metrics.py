56bcd3342c45bb253ad7958dadd34f406e69e7c8
Rachel Wang <rach.wang16@gmail.com>
2019-12-11 10:12:53 -0500

    setting up main for metric calculations

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import argparse
 
 ''' for calculating metrics '''
 
@@ -37,4 +38,10 @@ def calc_ssim(true_img_path, output_img_path):
 
     ssim = tf.image.ssim(true_img, output_img, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
     return ssim
+
+def main(args):
+    # parse args for file paths
+    print(calc_mse())
+    print(calc_psnr())
+    print(calc_ssim())
 
